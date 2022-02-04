bfe4d16440509448f5c136b1cf43fe85c0121d3a
Dan-Lucian <danugaina1403@gmail.com>
2022-02-04 16:30:54 +0200

    feat: ScrollToTop component to scroll when route changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 import PageAbout from './routes/about/PageAbout';
 import PageIndex from './routes/index/PageIndex';
@@ -10,6 +11,7 @@ import PageNonexistent from './routes/nonexistent/PageNonexistent';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<PageIndex />} />
