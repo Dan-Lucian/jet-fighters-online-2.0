3cbb73477dab5bfc9166048d8376b096bd8a17e0
Dan-Lucian <danugaina1403@gmail.com>
2022-02-12 10:49:20 +0200

    refactor: extracted PreLobby components to their own grid

--- frontend/src/routes/index/routes/index/PreLobby.js
+++ frontend/src/routes/index/routes/index/PreLobby.js
@@ -1,15 +1,17 @@
+// local components
+import WrapperPreLobby from './components/WrapperPreLobby';
 import BtnCreate from './components/BtnCreate';
 import BtnJoin from './components/BtnJoin';
 import StatusWs from './components/StatusWs';
 import FormId from './components/FormId';
 
 const PreLobby = () => (
-  <>
+  <WrapperPreLobby>
     <BtnCreate />
     <BtnJoin />
     <StatusWs />
     <FormId />
-  </>
+  </WrapperPreLobby>
 );
 
 export default PreLobby;
---
+++ frontend/src/routes/index/routes/index/components/WrapperPreLobby.js
@@ -0,0 +1,7 @@
+import styles from './WrapperPreLobby.module.scss';
+
+const WrapperPreLobby = (props) => (
+  <div className={styles.wrapper} {...props} />
+);
+
+export default WrapperPreLobby;
