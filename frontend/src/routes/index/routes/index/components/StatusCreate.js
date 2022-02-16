08c5bfd0296e5d054ca506fa2db0dd194ccd2211
Dan-Lucian <danugaina1403@gmail.com>
2022-02-16 16:54:57 +0200

    feat: statusCreate text linked to statusGame

--- frontend/src/routes/index/routes/index/components/StatusCreate.js
+++ frontend/src/routes/index/routes/index/components/StatusCreate.js
@@ -1,20 +1,23 @@
 // shared hooks
-import { useContextWebsocket } from '../../../../../providers/ProviderWebsocket';
+import { useContextGame } from '../../../../../providers/ProviderGame';
 
 // styles
 import styles from './StatusCreate.module.scss';
 
 const StatusCreate = () => {
-  const { message } = useContextWebsocket();
-  const { event, success, reason } = message;
+  const [game] = useContextGame();
 
-  let text = 'No lobby created';
+  const { statusGame } = game;
 
-  if (event === 'create' && !success && reason === 'lobbyAlreadyCreated') {
-    text = `You are in a lobby already`;
+  if (statusGame === 'lobby') {
+    return (
+      <div className={`${styles.statusCreate} ${styles.textGreen}`}>
+        Already in a lobby
+      </div>
+    );
   }
 
-  return <div className={styles.statusCreate}>{text}</div>;
+  return <div className={styles.statusCreate}>You are not in a lobby</div>;
 };
 
 export default StatusCreate;
