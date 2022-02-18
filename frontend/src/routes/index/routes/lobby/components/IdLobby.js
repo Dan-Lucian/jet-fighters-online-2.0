11185b8499834950f3e8ddacde4449d48607194f
Dan-Lucian <danugaina1403@gmail.com>
2022-02-18 19:31:08 +0200

    feat: lobby id copy on click

--- frontend/src/routes/index/routes/lobby/components/IdLobby.js
+++ frontend/src/routes/index/routes/lobby/components/IdLobby.js
@@ -1,11 +1,40 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // styles
 import styles from './IdLobby.module.scss';
 
-const IdLobby = ({ idLobby }) => (
-  <h2 className={styles.idLobby}>Lobby ID: {idLobby}</h2>
-);
+const IdLobby = ({ idLobby }) => {
+  const [isCopied, setIsCopied] = useState();
+
+  const getHandlerClick = () => {
+    if (isCopied) return null;
+
+    return () => {
+      navigator.clipboard.writeText(idLobby);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
+    };
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <h2 className={styles.title}>
+        Lobby ID:{' '}
+        <button
+          onClick={getHandlerClick()}
+          className={styles.btn}
+          type="button"
+        >
+          {isCopied && 'Copied'}
+          {!isCopied && idLobby}
+        </button>
+      </h2>
+    </div>
+  );
+};
 IdLobby.propTypes = {
   idLobby: PropTypes.string,
 };
