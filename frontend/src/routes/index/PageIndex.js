285de58a3138121f8994593307961dca2ea675f9
Dan-Lucian <danugaina1403@gmail.com>
2022-02-19 20:05:58 +0200

    refactor: removed Menu middleman component

--- frontend/src/routes/index/PageIndex.js
+++ frontend/src/routes/index/PageIndex.js
@@ -1,5 +1,28 @@
-import Menu from './components/Menu';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
-const PageIndex = () => <Menu />;
+// shared components
+import ErrorBoundary from '../../components/ErrorBoundary';
+import ErrorRouteFallback from '../../components/ErrorRouteFallback';
+import Loader from '../../components/Loader';
+
+// local components
+import Customization from './components/Customization';
+
+// styles
+import styles from './PageIndex.module.scss';
+
+const PageIndex = () => (
+  <main className={styles.menu}>
+    <ErrorBoundary FallbackComponent={ErrorRouteFallback}>
+      <Suspense
+        fallback={<Loader style={{ width: '41.5em', paddingTop: '1em' }} />}
+      >
+        <Outlet />
+      </Suspense>
+    </ErrorBoundary>
+    <Customization />
+  </main>
+);
 
 export default PageIndex;
