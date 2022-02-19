d8d93f2dd4141d2c3ce7be90546e93197c2aa2f0
Dan-Lucian <danugaina1403@gmail.com>
2022-02-19 20:20:25 +0200

    refactor: removed WrapperLobby styles middleman

--- frontend/src/routes/index/routes/lobby/Lobby.js
+++ frontend/src/routes/index/routes/lobby/Lobby.js
@@ -6,12 +6,14 @@ import { useLobbyWsEvents } from './hooks/useLobbyWsEvents';
 import { useUnmountWsMessage } from './hooks/useUnmountWsMessage';
 
 // local components
-import WrapperLobby from './components/WrapperLobby';
 import IdLobby from './components/IdLobby';
 import TablePlayers from './components/TablePlayers';
 import BtnReady from './components/BtnReady';
 import BtnStart from './components/BtnStart';
 import BtnQuit from './components/BtnQuit';
+
+// styles
+import styles from './Lobby.module.scss';
 
 const Lobby = () => {
   useLobbyWsEvents();
@@ -21,13 +23,13 @@ const Lobby = () => {
   const { idLobby } = lobby;
 
   return (
-    <WrapperLobby>
+    <div className={styles.wrapper}>
       <IdLobby idLobby={idLobby} />
       <TablePlayers />
       <BtnQuit />
       <BtnReady />
       <BtnStart />
-    </WrapperLobby>
+    </div>
   );
 };
 
--- frontend/src/routes/index/routes/lobby/components/WrapperLobby.js
+++
@@ -1,6 +0,0 @@
-// styles
-import styles from './WrapperLobby.module.scss';
-
-const WrapperLobby = (props) => <div className={styles.wrapper} {...props} />;
-
-export default WrapperLobby;
