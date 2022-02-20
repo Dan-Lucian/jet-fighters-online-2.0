63b47b48d6c0813b4197682fea87e8d64c9cc1aa
Dan-Lucian <danugaina1403@gmail.com>
2022-02-20 11:05:39 +0200

    change: idJoin sent by FormId instead of BtnJoin
    * did so in order to utilize native html form validation

--- frontend/src/routes/index/routes/index/components/BtnJoin.js
+++ frontend/src/routes/index/routes/index/components/BtnJoin.js
@@ -1,49 +1,24 @@
 // shared hooks
 import { useContextGame } from '../../../../../providers/ProviderGame';
-import { useContextUser } from '../../../../../providers/ProviderUser';
-import { useContextSettings } from '../../../../../providers/ProviderSettings';
-import { useContextWebsocket } from '../../../../../providers/ProviderWebsocket';
 
 // styles
 import styles from './BtnJoin.module.scss';
 
 const BtnJoin = () => {
   const [game] = useContextGame();
-  const [user] = useContextUser();
-  const [settings] = useContextSettings();
-  const { sendMessage } = useContextWebsocket();
 
   const { stateGame } = game;
-  const { name } = user;
-  const { idJoin } = settings;
 
   const isstateGamePreLobby = stateGame === 'preLobby';
 
-  const getHandlerClick = () => {
-    if (isstateGamePreLobby)
-      return () => {
-        sendMessage({
-          name,
-          idLobby: idJoin,
-          event: 'join',
-        });
-      };
-
-    return () =>
-      console.log(
-        `join denial because needed stateGame: preLobby but currently stateGame: ${stateGame}`
-      );
-  };
-
   return (
-    <button
+    <input
       disabled={!isstateGamePreLobby}
-      onClick={getHandlerClick()}
       className={styles.btnJoin}
-      type="button"
-    >
-      Join a lobby
-    </button>
+      type="submit"
+      value="Join a lobby"
+      form="form-id"
+    />
   );
 };
 
--- frontend/src/routes/index/routes/index/components/FormId.js
+++ frontend/src/routes/index/routes/index/components/FormId.js
@@ -1,16 +1,42 @@
-// shared components
+// shared hooks
+import { useContextGame } from '../../../../../providers/ProviderGame';
+import { useContextUser } from '../../../../../providers/ProviderUser';
 import { useContextSettings } from '../../../../../providers/ProviderSettings';
+import { useContextWebsocket } from '../../../../../providers/ProviderWebsocket';
 
 // styles
 import styles from './FormId.module.scss';
 
 const FormId = () => {
+  const [game] = useContextGame();
+  const [user] = useContextUser();
   const [settings, setSettings] = useContextSettings();
+  const { sendMessage } = useContextWebsocket();
+
+  const { stateGame } = game;
+  const { name } = user;
   const { idJoin } = settings;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Id submitted');
+  const isstateGamePreLobby = stateGame === 'preLobby';
+
+  const getHandlerSubmit = () => {
+    if (isstateGamePreLobby)
+      return (e) => {
+        e.preventDefault();
+        setSettings((prev) => ({ ...prev, idJoin: '' }));
+        sendMessage({
+          name,
+          idLobby: idJoin,
+          event: 'join',
+        });
+      };
+
+    return (e) => {
+      e.preventDefault();
+      console.log(
+        `join denial because needed stateGame: preLobby but currently stateGame: ${stateGame}`
+      );
+    };
   };
 
   const handleChange = (e) => {
@@ -18,13 +44,15 @@ const FormId = () => {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={getHandlerSubmit()} id="form-id">
       <input
         className={styles.input}
         type="text"
         placeholder="Insert room ID here"
         value={idJoin}
         onChange={handleChange}
+        pattern="^r[A-Za-z0-9]{6}$"
+        required
       />
     </form>
   );
