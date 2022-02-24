bb2182546572ee44ebbef4b8265dc5f6523dd667
Dan-Lucian <danugaina1403@gmail.com>
2022-02-24 11:17:55 +0200

    feat: lobby & game links hide if not correct stateApp

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,31 +1,49 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+// shared hooks
+import { useContextGlobal } from '../providers/ProviderGlobal';
+
+// shared components
 import TogglerTheme from './TogglerTheme';
 
+// styles
 import styles from './Nav.module.scss';
 
-const Nav = ({ theme, getTogglerTheme }) => (
-  <nav className={styles.nav}>
-    <Link to="/" className={styles.index}>
-      Jet Fighters Online
-    </Link>
-    <span className={styles.spacer} />
-    <Link to="/game" className={styles.about}>
-      Game
-    </Link>
-    <Link to="/lobby" className={styles.about}>
-      Lobby
-    </Link>
-    <Link to="/about" className={styles.about}>
-      About
-    </Link>
-    <Link to="/login" className={styles.signin}>
-      Sign in
-    </Link>
-    <TogglerTheme theme={theme} getTogglerTheme={getTogglerTheme} />
-  </nav>
-);
+const Nav = ({ theme, getTogglerTheme }) => {
+  const [global] = useContextGlobal();
+  const { stateApp } = global;
+
+  const isStateAppLobby = stateApp === 'lobby';
+  const isStateAppGame =
+    stateApp === 'game' || stateApp === 'countdown' || stateApp === 'gameOver';
+
+  return (
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.index}>
+        Jet Fighters Online
+      </Link>
+      <span className={styles.spacer} />
+      {isStateAppGame && (
+        <Link to="/game" className={styles.about}>
+          Game
+        </Link>
+      )}
+      {(isStateAppLobby || isStateAppGame) && (
+        <Link to="/lobby" className={styles.about}>
+          Lobby
+        </Link>
+      )}
+      <Link to="/about" className={styles.about}>
+        About
+      </Link>
+      <Link to="/login" className={styles.signin}>
+        Sign in
+      </Link>
+      <TogglerTheme theme={theme} getTogglerTheme={getTogglerTheme} />
+    </nav>
+  );
+};
 Nav.propTypes = {
   theme: PropTypes.string,
   getTogglerTheme: PropTypes.func,
