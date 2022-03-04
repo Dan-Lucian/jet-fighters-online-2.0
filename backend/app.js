c53ea402382e9df8ae4fe3a880fd90567774a366
Dan-Lucian <danugaina1403@gmail.com>
2022-03-04 16:18:18 +0200

    feat: setup mongodb connection
    * add express-async-errors library

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import middleware from './utils/middleware.js';
 import routerWild from './controllers/wild.js';
+import { MONGODB_URI } from './utils/config.js';
+import logger from './utils/logger.js';
 
 const app = express();
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 app.use(cors());
 app.use(express.static('build'));
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,4 +3,9 @@ import 'dotenv/config';
 
 const PORT = process.env.PORT;
 
-export { PORT };
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+
+export { PORT, MONGODB_URI };
