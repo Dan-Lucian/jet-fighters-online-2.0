9fea92de74addd5e215520dc649ab2c633fa57f9
Dan-Lucian <danugaina1403@gmail.com>
2022-03-15 17:11:55 +0200

    feat: profile page styles and logic

--- frontend/src/routes/profile/PageProfile.js
+++ frontend/src/routes/profile/PageProfile.js
@@ -1,9 +1,16 @@
-// shared hooks
 import { useNavigate } from 'react-router-dom';
+
+// shared hooks
 import { useContextAuth } from '../../providers/ProviderAuth';
 
 // shared components
 import PageNonexistent from '../../components/PageNonexistent';
+
+// styles
+import styles from './PageProfile.module.scss';
+
+// assets
+import { typesJet } from '../../config/typesJet';
 
 const PageProfile = () => {
   const navigate = useNavigate();
@@ -17,13 +24,107 @@ const PageProfile = () => {
   };
 
   return (
-    <main>
-      <h1>Hello {account.userName}</h1>
-      <button onClick={handleClick} type="button">
-        Log out
-      </button>
+    <main className={styles.wrapper}>
+      <div className={styles.jet}>
+        <img
+          className={styles.img}
+          src={typesJet[getJetMostPlayed(account.stats)].imgJet}
+          alt="jet"
+        />
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles.wrapperName}>
+          <h1 className={styles.name}>{account.userName}</h1>
+          <p className={styles.email}>{account.email}</p>
+          <p>{getFormattedTime(account.created)}</p>
+          <button
+            className={styles.btnLogout}
+            onClick={handleClick}
+            type="button"
+          >
+            Log out
+          </button>
+        </div>
+        <div className={styles.stats}>
+          <div className={styles.stat}>
+            <p>Wins</p>
+            <p>{account.stats.wins}</p>
+          </div>
+          <div className={styles.stat}>
+            <p>Loses</p>
+            <p>{account.stats.loses}</p>
+          </div>
+          <div className={styles.stat}>
+            <p>Draws</p>
+            <p>{account.stats.draws}</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
+};
+
+const getJetMostPlayed = (stats) => {
+  let max = -1;
+  let typeJet = '';
+
+  for (const [prop, value] of Object.entries(stats)) {
+    if (prop.startsWith('gamesWith')) {
+      if (value > max) {
+        max = value;
+        typeJet = prop;
+      }
+    }
+  }
+
+  switch (typeJet) {
+    case 'gamesWithBalanced':
+      return 'balanced';
+
+    case 'gamesWithSpeedster':
+      return 'speedster';
+
+    case 'gamesWithTrickster':
+      return 'trickster';
+
+    case 'gamesWithTank':
+      return 'tank';
+
+    case 'gamesWithLongLaster':
+      return 'long-laster';
+
+    case 'gamesWithFastBullet':
+      return 'fast-bullet';
+
+    default:
+      console.error('No such jet');
+      return 'balanced';
+  }
+};
+
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const getFormattedTime = (time) => {
+  const date = new Date(time);
+  const text =
+    `Joined ${date.getUTCDate()} ` +
+    `${monthNames[date.getUTCMonth()]} ${date.getUTCFullYear()}`;
+
+  return text;
 };
 
 export default PageProfile;
