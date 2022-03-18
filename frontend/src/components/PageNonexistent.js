bc32b5d4506837ad54c8c25e61267bac11eb55f2
Dan-Lucian <danugaina1403@gmail.com>
2022-03-18 19:00:01 +0200

    feat: styled 404 page

--- frontend/src/components/PageNonexistent.js
+++ frontend/src/components/PageNonexistent.js
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
+// styles
 import styles from './PageNonexistent.module.scss';
 
 const PageNonexistent = () => (
-  <div className={styles.pageNonexistent}>PageNonexistent</div>
+  <main className={styles.wrapper}>
+    <div>404 no page here</div>
+
+    <Link className={styles.link} to="/">
+      home
+    </Link>
+  </main>
 );
 
 export default PageNonexistent;
