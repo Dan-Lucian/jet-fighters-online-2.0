66a4b5f12d6c653a66c7f9dd80f12c2ec137c347
Dan-Lucian <danugaina1403@gmail.com>
2022-03-19 12:32:35 +0200

    chore: test machine sti delay

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,25 @@ server.listen(PORT, () => {
 
 // put on all the websocket related stuff on the server
 websocket(server);
+
+const hrtimeMs = function () {
+  const time = process.hrtime();
+  return time[0] * 1000 + time[1] / 1000000;
+};
+
+const TICK_RATE = 20;
+let tick = 0;
+let previous = hrtimeMs();
+const tickLengthMs = 1000 / TICK_RATE;
+
+const loop = () => {
+  setTimeout(loop, tickLengthMs);
+  const now = hrtimeMs();
+  const delta = (now - previous) / 1000;
+  console.log('delta', delta);
+  // game.update(delta, tick) // game logic would go here
+  previous = now;
+  tick += 1;
+};
+
+loop(); // starts the loop
