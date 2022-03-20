c0a0b54d1827f76455726d666968c6958d002067
Dan-Lucian <danugaina1403@gmail.com>
2022-03-20 13:25:12 +0200

    chore: activate console.error on prod

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -28,7 +28,6 @@ const PageNonexistent = lazy(() => import('./components/PageNonexistent'));
 // replace console.* for disable log on production
 if (process.env.NODE_ENV === 'production') {
   console.log = () => {};
-  console.error = () => {};
   console.debug = () => {};
 }
 
