d0154caee0c8dac1bba82b0fa97af738656ddb4a
Dan-Lucian <danugaina1403@gmail.com>
2022-03-30 13:26:34 +0300

    fix: logging file requests

--- backend/middleware/loggerRequest.js
+++ backend/middleware/loggerRequest.js
@@ -1,12 +1,25 @@
 const morgan = require('morgan');
 
+const skipLog = (req, res) => {
+  const { url } = req;
+
+  if (
+    url.match(/(js|jpg|png|ico|css|woff|woff2|eot|webp|svg|refresh-token)$/gi)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
 morgan.token(
   'ip',
   (req) => req.headers['x-forwarded-for'] || req.connection.remoteAddress
 );
 
 const loggerRequest = morgan(
-  ':ip :method :url :status :res[content-length] - :response-time ms'
+  ':ip :method :url :status :res[content-length] - :response-time ms',
+  { skip: skipLog }
 );
 
 module.exports = loggerRequest;
