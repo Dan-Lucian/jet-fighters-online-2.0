4bb552fbdedcf144fe9deb374eddfe5b3c0e7371
Dan-Lucian <danugaina1403@gmail.com>
2022-09-10 12:48:52 +0300

    [chore] ts setup

---
+++ frontend/src/components/BtnSubmit/BtnSubmit.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+// styles
+import styles from './BtnSubmit.module.scss';
+
+interface Props {
+  children: ReactNode;
+  disabled: boolean;
+}
+
+const BtnSubmit = ({ disabled, children }: Props) => (
+  <button disabled={disabled} type="submit" className={styles.btn}>
+    {children}
+  </button>
+);
+
+export default BtnSubmit;
